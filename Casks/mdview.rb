3ddd2eae6d24b6b77913c071bb6b3d2f3e9b8eea
Owen Rumney <owen@owenrumney.co.uk>
2026-05-03 19:57:13 +0100

    Update mdview.rb

--- Casks/mdview.rb
+++ Casks/mdview.rb
@@ -12,12 +12,12 @@ cask "mdview" do
     on_intel do
       url "https://github.com/owenrumney/mdview/releases/download/v#{version}/mdview_darwin_amd64.tar.gz"
       sha256 "8634f902418fa9d14836d371023ca6d162b06ccf963b7687d158d5de429f88c9"
-      binary "lazypprof"
+      binary "mdview"
     end
     on_arm do
       url "https://github.com/owenrumney/mdview/releases/download/v#{version}/mdview_darwin_arm64.tar.gz"
       sha256 "035bad1311f244f7210b8a3c265aea70015bfba5651fbf4b3838bde9675ff357"
-      binary "lazypprof"
+      binary "mdview"
     end
   end
 
@@ -25,12 +25,12 @@ cask "mdview" do
     on_intel do
       url "https://github.com/owenrumney/mdview/releases/download/v#{version}/mdview_linux_amd64.tar.gz"
       sha256 "961828fd4d97f8ed465ddd876464372726b41401a99ee8e6786f31afcda92796"
-      binary "lazypprof"
+      binary "mdview"
     end
     on_arm do
       url "https://github.com/owenrumney/mdview/releases/download/v#{version}/mdview_linux_arm64.tar.gz"
       sha256 "3090a8661e0258e652c1d56c468735b4705648a3f6b9bda9cbae1db67a5cb5d2"
-      binary "lazypprof"
+      binary "mdview"
     end
   end
 
